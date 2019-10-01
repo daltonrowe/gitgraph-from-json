3dd4ca0db48ca4e502b9912040a4c328941e8c01
Dalton Rowe <cdaltonrowe@gmail.com>
2019-10-01 17:41:54 -0500

    json export

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const fs = require("fs");
 const git2json = require("@fabien0102/git2json");
 
-git2json.run().then(myGitLogJSON => console.log(myGitLogJSON));
+const repoPath = "./";
+
+function writeToJSON(json) {
+  fs.writeFile("output.json", json, "utf8", function(err) {
+    if (err) {
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
+    }
+
+    console.log("JSON file has been saved.");
+  });
+}
+
+git2json.run({ repoPath }).then(myGitLogJSON => {
+  writeToJSON(JSON.stringify(myGitLogJSON));
+  console.log("Done!");
+});
