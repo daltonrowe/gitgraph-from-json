5e16c6266ae6ad28e9232556af0b88f2812e40e8
Dalton Rowe <cdaltonrowe@gmail.com>
2019-10-01 19:20:55 -0500

    werks

---
+++ graph.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  function ajax(url) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        resolve(this.responseText);
+      };
+      xhr.onerror = reject;
+      xhr.open("GET", url);
+      xhr.send();
+    });
+  }
+
+  ajax("output.json")
+    .then(function(result) {
+      const graphContainer = document.getElementById("graph-container");
+      const gitgraph = GitgraphJS.createGitgraph(graphContainer);
+      gitgraph.import(JSON.parse(result));
+    })
+    .catch(function(err) {
+      console.log(err, "error");
+    });
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Serve our example pages as the root
+app.use(
+  "/",
+  express.static(path.join(__dirname, "./"), {
+    extensions: ["htm", "html"] // Allow use of links without extensions
+  })
+);
+
+// Serve Turbolinks source code
+app.use(
+  "/text",
+  express.static(path.join(__dirname, "./"), {
+    setHeaders: function(res, path, stat) {
+      res.set("Content-Type", "text/plain"); // Set headers to text plain for easier frontend
+    }
+  })
+);
+
+// Hey, Listen!
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
